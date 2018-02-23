aecb903623c0a618ada31189a52de13ca75edfe9
Becca Stanger <beccastanger@gmail.com>
2018-02-22 16:02:09 -0800

    Fix some data and add random series info

--- workers/generateData.js
+++ workers/generateData.js
@@ -1,32 +1,35 @@
 const fs = require('fs');
 const mongoose = require('mongoose');
-
 const timeLocationModel = require('./../db/models/timeLocation.js');
 
 mongoose.connect('mongodb://localhost/timeLocations');
 
-var jsonData = fs.readFileSync('./150UpcomingEvents.json');
-var parsedData = JSON.parse(jsonData);
+let jsonData = fs.readFileSync('./150UpcomingEvents.json');
+let parsedData = JSON.parse(jsonData);
 parsedData.events.forEach(function(event){
 
   // Need compute start and end time here
-  // Need pick occasional weekly or monthly series value here
+  let seriesValues = [null, null, null, null, null, "weekly", "monthly"];
+  let randomIdx = Math.floor(Math.random() * seriesValues.length);
 
   let structuredEvent = {
     event_id: event.id,
     // start_time: event.local_time || event.time,
+    // end_time: event.local_time || event.time,
+    series: seriesValues[randomIdx],
     venue_public: false
   }
   if(event.venue){
     structuredEvent.venue_public = true;
-    structuredEvent.venue_name = event.name;
-    if(event.address_line1) structuredEvent.address_line1 = event.address_line1;
-    if(event.address_line2) structuredEvent.address_line2 = event.address_line2;
-    if(event.address_line3) structuredEvent.address_line3 = event.address_line3;
-    structuredEvent.city = event.city;
-    structuredEvent.state = event.state;
-    if(event.lat) structuredEvent.lat = event.lat;
-    if(event.lon) structuredEvent.lat = event.lon;
+    structuredEvent.venue_name = event.venue.name;
+    if(event.venue.address_line1) structuredEvent.address_line1 = event.venue.address_line1;
+    if(event.venue.address_line2) structuredEvent.address_line2 = event.venue.address_line2;
+    if(event.venue.address_line3) structuredEvent.address_line3 = event.venue.address_line3;
+    structuredEvent.city = event.venue.city;
+    structuredEvent.state = event.venue.state;
+    // console.log(event.venue);
+    if(event.venue.lon) structuredEvent.longitude = event.venue.lon;
+    if(event.venue.lat) structuredEvent.latitude = event.venue.lat;
   }
 
   timeLocationModel.insertModel(structuredEvent, function(err){
@@ -36,24 +39,5 @@ parsedData.events.forEach(function(event){
       console.log('inserted!');
     }
   });
-  // console.log(event.link);
-  // console.log(event.id);
-  // console.log(event.local_date);
-  // console.log(event.local_time);
-  // if(!event.local_time){
-  //   console.log('no local_time');
-  //   console.log('but time is ', event.time);
-  // }
-  // console.log(event.duration);
-  // if(event.venue){
-  //   console.log(event.venue.name);
-  //   console.log(event.venue.address_1);
-  //   console.log(event.venue.address_2);
-  //   console.log(event.venue.address_3);
-  //   console.log(event.venue.city);
-  //   console.log(event.venue.state);
-  //   console.log(event.venue.lat);
-  //   console.log(event.venue.lon);
-  // }
-  // console.log('//////////////////////////////');
-})
+});
+// mongoose.disconnect();
