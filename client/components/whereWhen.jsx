eef10ca92a1ea168cdd1cd537e6d0c7e31d0d01e
Becca Stanger <beccastanger@gmail.com>
2018-02-27 10:15:04 -0800

    Lay down basic layout with mock data

--- client/components/whereWhen.jsx
+++ client/components/whereWhen.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import When from './when.jsx'
+import Where from './where.jsx'
 // import styles from './styles/styles.css'
 
 let WhereWhen = (props) => (
   <div className="whereWhen">
+    <When />
+    <Where />
   </div>
 );
 
