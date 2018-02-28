ffd426cc580105195a74ba4510ca68be4f7e686f
Becca Stanger <beccastanger@gmail.com>
2018-02-28 11:58:51 -0800

    Add basic enzyme test

---
+++ __tests__/renderComponents.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import expect from 'expect'
+import { shallow, mount, render } from 'enzyme'
+import WhereWhen from './../client/components/whereWhen.jsx'
+
+describe('<WhereWhen />', () => {
+ it('should be defined', () => {
+   expect(WhereWhen).toBeDefined();
+ });
+});
