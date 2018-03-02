1bef844f53dfdb78195e78db36307a16d1e013ba
Becca Stanger <beccastanger@gmail.com>
2018-03-02 12:07:07 -0800

    Reformat code to use Airbnb style guide

--- client/components/whereWhen.jsx
+++ client/components/whereWhen.jsx
@@ -1,40 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import When from './when.jsx'
-import Where from './where.jsx'
+import React from 'react';
+import When from './when.jsx';
+import Where from './where.jsx';
 // import styles from './styles/styles.css'
 
 export default class WhereWhen extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       whereData: {},
-      whenData: {}
-    }
+      whenData: {},
+    };
   }
 
-  componentDidMount(){
-    let url = window.location.href;
-    let urlEnd = url.split('/event/')[1];
-    let eventId = urlEnd.split('/')[0];
+  componentDidMount() {
+    const url = window.location.href;
+    const urlEnd = url.split('/event/')[1];
+    const eventId = urlEnd.split('/')[0];
     fetch('/api/event/' + eventId).then((response) => {
       return response.json();
     }).then((jsonData) => {
-        this.setState({
-          whereData: jsonData.whereData,
-          whenData: jsonData.whenData
-        })
+      this.setState({
+        whereData: jsonData.whereData,
+        whenData: jsonData.whenData,
+      });
     }).catch((err) => {
-        throw new Error(err);
+      throw new Error(err);
     });
   }
 
-  render(){
+  render() {
     return (
       <div className="whereWhen">
-        <When whenData={this.state.whenData}/>
+        <When whenData={this.state.whenData} />
         <Where whereData={this.state.whereData} />
       </div>
-    )
+    );
   }
 }
