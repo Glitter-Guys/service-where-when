fa9cfe58e1870786b92eddffa5689463bd328b99
Becca Stanger <beccastanger@gmail.com>
2018-03-04 08:37:35 -0800

    Add tests for Map component

--- client/components/map.jsx
+++ client/components/map.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { googleMapsKey } from './../../config/apiKeys';
 
 const Map = (props) => {
-  const googleMapsCall = `https://maps.googleapis.com/maps/api/staticmap?key=${googleMapsKey}&center=${props.latitude}, ${props.longitude}&size=480x300&zoom=17&scale=2&markers=color%3Ared%7Csize%3Alarge%7C37.802914%2C-122.412239&sensor=false`;
+  const googleMapsCall = `https://maps.googleapis.com/maps/api/staticmap?key=${googleMapsKey}&center=${props.latitude},${props.longitude}&size=480x300&zoom=17&scale=2&markers=color%3Ared%7Csize%3Alarge%7C`;
   const addressToQuery = `${props.whereData.address1}, ${props.whereData.city}, ${props.whereData.state}`;
   const encodedAddress = encodeURIComponent(addressToQuery);
   const googleMapsLink = `https://www.google.com/maps/search/?api=1&query=${encodedAddress}`;
 
   return (
     <div className="map">
-      <a href={googleMapsLink} target="_blank">
+      <a className="map__link" href={googleMapsLink} target="_blank">
         <img className="map__img" alt="map of location" src={googleMapsCall} />
       </a>
     </div>
